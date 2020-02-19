e97c017c74ae95d85f252309bf85a93267c4502b
Kat <kat@pop-os.localdomain>
2020-02-19 15:37:46 +0100

    Fixed a bug.

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,7 @@ execute(message, args, prefix) {
 if(!message.content.startsWith(prefix)||message.author.bot) return;
 var input = message.content
 var userinput = input.substr(6)
+if(message.content.startsWith(`${prefix}say ${prefix}`)) return message.channel.send('please don\'t exploit me :(((')
 message.delete(1000)
 message.channel.send(userinput)
 }
