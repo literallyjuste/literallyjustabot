8a4faf28223be00466c9daf58d5b99be5f8e7c3e
literallyjuste <literallyjuste@gmail.com>
2020-03-09 02:24:15 +0100

    Added support for her lover

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ name: 'say',
 description: "Make the bot say something.",
 usage: "[text]",
 execute(message, args, prefix) {
-if(!message.content.startsWith(prefix)||message.author.bot) return;
+if(!message.content.startsWith(prefix)||message.author.bot&& message.author.id != "672280373065154569") return;
 var input = message.content
 var userinput = input.substr(6)
 if(message.content.startsWith(`${prefix}say ${prefix}`)) return message.channel.send('please don\'t exploit me :(((')
